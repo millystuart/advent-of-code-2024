ac6cb0ce8a6150229460261c4a7ab735bb5ab5de
ms3n22 <melissaaa.stuart@gmail.com>
2024-12-13 09:00:24 +0000

    Complete day 2 half 2

--- day2/main.py
+++ day2/main.py
@@ -28,3 +28,40 @@ file.close()
 print(safe_tally)
 
 # ------------------------------ Part 2 ------------------------------ #
+
+file = open("/home/milly/Code/python/advent-of-code-2024/day2/input.txt", "r")
+safe_tally2 = 0
+
+# function to check if a line is valid with problem dampener
+def problem_dampener_check(line):
+    # usual check from part 1
+    if line == sorted(line) or line == sorted(line, reverse=True):
+        if check_safe_levels(line):
+            return True
+
+    # Check if removing a single number makes the line valid
+    for i in range(0, len(line)):
+        modified_line = []
+        for j in range (0, len(line)):
+            if j != i:
+                modified_line.append(line[j])
+        
+        if modified_line == sorted(modified_line) or modified_line == sorted(modified_line, reverse=True):
+            if check_safe_levels(modified_line):
+                return True
+
+    return False
+
+for line in file:
+    split_line = line.split()
+
+    # cast all strings in list as ints
+    line = list(map(int, split_line))
+
+    # check if the line is valid
+    if problem_dampener_check(line) == True:
+        safe_tally2 += 1
+
+file.close()
+
+print(safe_tally2) 
